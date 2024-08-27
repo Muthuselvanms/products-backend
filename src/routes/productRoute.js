ed83cd4521bcd34e84190fdf388dafdb1b0a57b6
Muthuselvanms <manomuthamizh1997@gmail.com>
2024-08-27 10:37:29 +0530

    product backend application

---
+++ src/routes/productRoute.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const productModel = require("../model/productModel")
+
+const productRouter = express.Router()
+
+productRouter.get('/products',async(req,res)=>{
+    try {
+        const allProducts = await productModel.find()
+        res.send(allProducts)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+productRouter.post('/post-product',async(req,res)=>{
+    const {name,brand,desc,image,price} = req.body
+    try {
+        const newProduct = new productModel({name,brand,desc,image,price})
+        await newProduct.save()
+        res.send(newProduct)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+productRouter.get('/products/:id',async(req,res)=>{
+    const id = req.params.id
+try {
+    const selectedProduct = await productModel.findById(id)
+    res.send(selectedProduct)
+} catch (error) {
+    res.send(error)
+}
+
+})
+productRouter.put('/product/:id',async(req,res)=>{
+    const id = req.params.id
+    const {name,brand,desc,image,price} = req.body
+    try {
+    await productModel.findByIdAndUpdate(id,{name,brand,desc,image,price})
+    res.send({message:"product updated sucessfully"})
+} catch (error) {
+    res.send(error)
+}
+})
+productRouter.delete('/product/:id',async(req,res)=>{
+    const id = req.params.id
+    try {
+        await productModel.findByIdAndDelete(id)
+        res.send({message:"product deleted successfully"})
+    } catch (error) {
+        res.send(error)
+    }
+})
+module.exports = productRouter
